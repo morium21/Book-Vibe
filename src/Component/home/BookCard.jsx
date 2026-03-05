798508ee3f4f269aedfb859ba5d1e8bda7ccccbe
Morium Akter <moriumakter912003@gmail.com>
2026-03-05 23:53:29 +0600

    Design book card section layout with image, tags and title

--- src/Component/home/BookCard.jsx
+++ src/Component/home/BookCard.jsx
@@ -7,33 +7,66 @@ import React from 'react'
 const BookCard = () => {
     return (
 
-    
 
-        
-<section className="">
 
- <h1 className="text-center font-bold text-4xl my-10">Books</h1>
-          {/* image */}
-        <div className="border border-gray-200 rounded-xl p-3 bg-white hover:shadow-md transition duration-300">
 
-            <div className="bg-gray-100 rounded-lg h-48 flex items-center">
-                <img 
-                src="Book Vibe/src/assets/book2.png" 
-                alt="book" 
-                className="h-40 object-contain"
-                />
+        <section className="">
+
+            <div>
+                <h1 className="text-center font-bold text-4xl my-10">Books</h1>
             </div>
 
-            {/* Tags */}
-            <div className="flex gap-2 mt-4">
+            {/* Card-1 */}
+            {/* image */}
+            <div className="border border-gray-200 rounded-xl p-3 bg-white hover:shadow-md transition duration-300">
+
+                <div className="bg-gray-100 rounded-lg h-48 flex items-center">
+                    <img
+                        src="Book Vibe/src/assets/book2.png"
+                        alt="book"
+                        className="h-40 object-contain"
+                    />
+                </div>
+
+                {/* Tags */}
+                <div className="flex gap-2 mt-4">
+                    <span className="text-xs bg-green-100 text-green-600 px-2 py-1 rounded-full">
+                        Young Adult
+                    </span>
+                    <span className="text-xs bg-green-100 text-green-600 px-2 py-1 rounded-full">
+                        Identity
+                    </span>
+                </div>
+
+
+                {/* Title */}
+                <h3 className="font-semibold text-lg mt-3">
+                    The Catcher  in the Rye
+                </h3>
+
+                {/* Author */}
+                <p className="text-sm text-gray-400 mt-1">
+                    By : Authore Name
+                </p>
+
+                {/* Bottom Info */}
+                <div className="border-t mt-4 pt-3 flex justify-between text-sm text-gray-500">
+                    <span>Fiction</span>
+                    <span>5.00  </span>
+
+
+                </div>
 
             </div>
 
-        </div>
-</section>
 
- 
-                                                                                                       
+
+
+
+        </section>
+
+
+
 
 
 
