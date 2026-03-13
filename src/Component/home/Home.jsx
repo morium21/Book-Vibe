1bbb0a7e80a1b65b8d79ab39e847b1b698057aba
morium21 <moriumakter912003@gmail.com>
2026-03-14 02:18:45 +0600

    complete home page card section

--- src/Component/home/Home.jsx
+++ src/Component/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroSection from './HeroSection'
 import Navbar from './Navbar'
 import Books from './Books'
-import BookCard from './BookCard'
+
 
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
         <>
             <Navbar />
             <HeroSection />
-            <BookCard />
+          
             <Books/>
 
         </>
