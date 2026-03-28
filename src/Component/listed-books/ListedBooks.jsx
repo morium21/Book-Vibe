80c3753f653c18d8286980b029e937fa34c06af1
morium21 <moriumakter912003@gmail.com>
2026-03-28 23:43:14 +0600

    card section add in listed books page

--- src/Component/listed-books/ListedBooks.jsx
+++ src/Component/listed-books/ListedBooks.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 import Navbar from '../home/Navbar'
 
+const books = [
+  {
+    id: 1,
+    title: "The Great Gatsby",
+    author: "Awlad Hossain",
+    image: "https://i.ibb.co.com/4fTnG7v/book1.png",
+    Rating: "4.5",
+    category: "Classic",
+    tags: ["Young Adult", "identity"],
+    publisher: "Scribner",
+    year: "1925",
+    pages: "180",
+
+  },
+]
+
 export const ListedBooks = () => {
   return (
     <>
@@ -12,11 +28,57 @@ export const ListedBooks = () => {
 
       <div className="text-center mb-6">
         <button className="bg-green-500 text-white px-8 py-3 rounded">
-          Sort By
-
+          Sort By  ▼
         </button>
+      </div>
+
+      <div className="flex gap-4 border-b pb-2 mb-6">
+        <button className="text-sm text-gray-600 border border-gray-500">
+          Read Books
+        </button>
+        <button className="text-sm text-gray-400">Wishlist Books</button>
+      </div>
+
+
+      <div className="space-y-5">
+        {books.map((book) => (
+          <div
+            key={book.id}
+            className="flex gap-6 border rounded-xl p-4 shadow-sm"
+          >
+            <img
+              src={book.img}
+              alt={book.title}
+              className="w-24 h-32 object-cover rounded"
+            />
+            <div className="flex-1">
+              <h3 className="font-semibold text-gray-800">
+                {book.title}
+              </h3>
+              <p className="text-sm text-gray-500 mb-2">
+                By : {book.author}
+              </p>
+
+              <div className="flex flex-wrap gap-2 text-xs mb-3">
+                <span className="bg-green-100 text-green-600 px-2 py-1 rounded">
+                  Rating {book.Rating}
+                 </span>
+                 <span className="bg-blue-100 text-blue-600 px-2 py-1 rounded">
+                  Pages {book.pages}
+                 </span>
+
+              </div>
+
+
+
+            </div>
+          </div>
+
+        ))}
 
       </div>
+
+
 
 
 
