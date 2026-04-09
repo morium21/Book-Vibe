a2f8c06ccba43ff5ebce924ef75836a2c5a19878
morium21 <moriumakter912003@gmail.com>
2026-04-09 11:59:14 +0600

    Added signup page in project

--- src/Component/sign-in/SignIn.jsx
+++ src/Component/sign-in/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 
 export const SignIn = () => {
@@ -16,16 +17,16 @@ export const SignIn = () => {
           <div>
             <label className="block text-gray-700 mb-2 font-medium">Email</label>
             <input type="email"
-            placeholder="Enter your email"
-            className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-green-400"
+              placeholder="Enter your email"
+              className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-green-400"
             />
           </div>
 
           <div>
             <label className="block text-gray-700 mb-2 font-medium">Password</label>
-            <input type="password" 
-            placeholder="Enter your password"
-            className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-green-400"
+            <input type="password"
+              placeholder="Enter your password"
+              className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-green-400"
             />
           </div>
 
@@ -33,15 +34,32 @@ export const SignIn = () => {
             <label className="flex items-center gap-2">
               <input type="checkbox" />
               Remember me
-
             </label>
-
+            <a href="#" className="text-green-500 hover:underline">
+              Forgot Password?
+            </a>
           </div>
 
+          <button
+            type="submit"
+            className="w-full bg-green-500 hover:bg-green-600 text-white py-3 rounded-lg font-semibold transition"
+          >
+            Sign In
+          </button>
         </form>
 
+        <p className="text-center text-gray-600 mt-6">
+          Don’t have an account?{" "}
+          <Link to="/signup" className="text-cyan-500 font-semibold hover:underline">
+            Sign Up
+          </Link>
+        </p>
       </div>
-
     </div>
   )
 }
+
+export default SignIn;
+
+
+
--- src/Component/sign-up/SignUp.jsx
+++ src/Component/sign-up/SignUp.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className="min-h-screen bg-gradient-to-r from-cyan-50 to-green-50 flex items-center justify-center px-4">
+      <div className="bg-white shadow-xl rounded--2xl w-full max-w-md p-8">
+        <h2 className="text-3xl font-bold text-center text-gray-800 mb-2">
+          Create Account
+          </h2>
+          <p className="text-center text-gray-500 mb-6">
+            Join Book Vibe and start your reading journey
+          </p>
+
+          <form className="space-y-5">
+            <div>
+              <label className="block text-gray-700 mb-2 font-medium">Full Name</label>
+              <input 
+              type="text" 
+              placeholder="Enter your full name"
+              className="w-full border border-gray-300 rounded-lg px-4 py-3 outline-none focus:ring-2 focus:ring-cyan-400"
+              />
+            </div>
+
+          </form>
+      </div>
+
+    </div>
   )
 }
