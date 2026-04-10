24d70fdb72fd0a0a3797966e64974dee1ae70060
morium21 <moriumakter912003@gmail.com>
2026-04-10 23:41:35 +0600

    complete sign in page design

--- src/Component/home/Navbar.jsx
+++ src/Component/home/Navbar.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { Menu, X } from "lucide-react"
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center px-10 py-5">
-            <h1 className="font-bold text-xl">Book Vibe</h1>
+        <div className="w-11/12 max-w-7xl mx-auto py-4">
+            <nav className="flex flex-col md:flex-row gap-4 justify-between items-center px-10 py-5">
+                <h1 className="font-bold text-xl">Book Vibe</h1>
 
-            <ul className="flex gap-6 text-gray-600">
-                <li><NavLink to={"/"}>Home</NavLink></li>
-                <li><NavLink to={"/listed-books"}>Listed Books</NavLink></li>
-                <li><NavLink to={"/page-to-read"}>Pages to Read</NavLink></li>
-               
-                
-            </ul>
+                <ul className="flex gap-6 text-gray-600">
+                    <li><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"/listed-books"}>Listed Books</NavLink></li>
+                    <li><NavLink to={"/page-to-read"}>Pages to Read</NavLink></li>
 
 
-            <div className="space-x-3">
-              <Link to={"/Sign-In"}>
-                <button className="bg-green-500 text-white px-4 py-2 rounded">
-                    Sign In
+                </ul>
 
-                </button>
-              </Link>
 
-             <Link to={"/Sign-Up"}>
-                <button className="bg-cyan-400 text-white px-4 py-2 rounded">
-                    Sign Up
+                <div className="space-x-3">
+                    <Link to={"/Sign-In"}>
+                        <button className="bg-green-500 text-white px-4 py-2 rounded">
+                            Sign In
 
-                </button>
-             </Link>
+                        </button>
+                    </Link>
 
-            </div>
+                    <Link to={"/Sign-Up"}>
+                        <button className="bg-cyan-400 text-white px-4 py-2 rounded">
+                            Sign Up
 
-        </nav>
+                        </button>
+                    </Link>
+
+                </div>
+
+            </nav>
+
+        </div>
     )
 }
 
